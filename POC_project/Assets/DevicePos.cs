b9301a439d30f0aa2cc783d375547a86de44c573
tobitotaku <99360631+tobitotaku@users.noreply.github.com>
2023-06-12 22:53:02 +0200

    refactor DevicePos script

--- POC_project/Assets/DevicePos.cs
+++ POC_project/Assets/DevicePos.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Net.Http;
 using UnityEngine;
 using UnityEngine.Networking;
 using static System.Net.Mime.MediaTypeNames;
@@ -11,27 +12,46 @@ using static System.Net.Mime.MediaTypeNames;
 public class DevicePos : MonoBehaviour
 {
     public Transform obj_transform;
-
+    public string ip = $"192.168.18.4";
+    public string port = $"32773";
+    private string url => $"http://{ip}:{port}/log?x={obj_transform.position}&y={obj_transform.rotation}&z=1";
     // Start is called before the first frame update
     void Start()
     {
         //Debug.Log("test");
     }
 
-    IEnumerator postRequest()
+    IEnumerator UnityGetRequest()
     {
-        string ip = $"192.168.18.4";
-        string port = $"32775";
-        string url = $"http://{ip}:{port}/log?x={obj_transform.position}&y={obj_transform.rotation}&z=1";
-        WWWForm form = new WWWForm();
+        //WWWForm form = new WWWForm();
 
-        UnityWebRequest uwr = UnityWebRequest.Post(url, form);
+        UnityWebRequest uwr = UnityWebRequest.Get(url);
         yield return uwr.SendWebRequest();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator NetGetRequest()
     {
-        StartCoroutine(postRequest());
+        HttpClient httpClient = new HttpClient();
+        yield return httpClient.GetAsync(url);
+    }
+
+    // Update is called once per frame
+    // use for translate/rotation/animations/input
+    //void Update()
+    //{
+
+    //}
+
+    // use for Forces/input
+    //void FixedUpdate()
+    //{
+
+    //}
+
+    // use for camera translation/transform manipulation post animation
+    void LateUpdate()
+    {
+        StartCoroutine(UnityGetRequest());
+        //StartCoroutine(NetGetRequest());
     }
 }
